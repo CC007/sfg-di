dda25681164a18a8c5be89ef22848f338bf78449
CC007 <Coolcat_the_best2@hotmail.com>
2020-12-01 23:48:38 +0100

    Section 6 video 87: create properties and fake datasource bean that uses them

--- src/main/java/com/github/cc007/sfgdi/SfgDiApplication.java
+++ src/main/java/com/github/cc007/sfgdi/SfgDiApplication.java
@@ -1,10 +1,7 @@
 package com.github.cc007.sfgdi;
 
-import com.github.cc007.sfgdi.controllers.ConstructorInjectedController;
-import com.github.cc007.sfgdi.controllers.I18nController;
 import com.github.cc007.sfgdi.controllers.MyController;
-import com.github.cc007.sfgdi.controllers.PropertyInjectedController;
-import com.github.cc007.sfgdi.controllers.SetterInjectedController;
+import com.github.cc007.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,25 +14,10 @@ public class SfgDiApplication
 	{
 
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-
-		I18nController controller = ctx.getBean("i18nController", I18nController.class);
-		System.out.println(controller.sayHello());
-
 		MyController myController = ctx.getBean("myController", MyController.class);
-		System.out.println("----- Primary Bean");
-		System.out.println(myController.sayHello());
 
-		System.out.println("----- Property");
-		PropertyInjectedController propertyInjectedController = ctx.getBean("propertyInjectedController", PropertyInjectedController.class);
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("----- Setter");
-		SetterInjectedController setterInjectedController = ctx.getBean("setterInjectedController", SetterInjectedController.class);
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("----- Constructor");
-		ConstructorInjectedController constructorInjectedController = ctx.getBean("constructorInjectedController", ConstructorInjectedController.class);
-		System.out.println(constructorInjectedController.getGreeting());
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
 	}
 
 }
