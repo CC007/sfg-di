1145be20ea25507bc9bcfeb77c58ca3df4443e4c
CC007 <Coolcat_the_best2@hotmail.com>
2020-12-01 23:53:28 +0100

    Section 6 video 88: use environment variable instead of properties file

--- src/main/java/com/github/cc007/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/github/cc007/sfgdi/config/PropertyConfig.java
@@ -1,16 +1,20 @@
 package com.github.cc007.sfgdi.config;
 
 import com.github.cc007.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig
 {
+	@Autowired
+	Environment environment;
 
 	@Value("${guru.username}")
 	String user;
@@ -24,7 +28,8 @@ public class PropertyConfig
 	@Bean
 	public FakeDataSource fakeDataSource(){
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		// or set the GURU_USERNAME env variable to set the guru.username value
+		fakeDataSource.setUser(environment.getProperty("USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
