76da0d5ded76080768c8eab47cf9b03cedb6e593
CC007 <Coolcat_the_best2@hotmail.com>
2020-12-02 00:13:36 +0100

    Section 6 video 90: refactor to only use application.properties

--- src/main/java/com/github/cc007/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/github/cc007/sfgdi/config/PropertyConfig.java
@@ -2,22 +2,13 @@ package com.github.cc007.sfgdi.config;
 
 import com.github.cc007.sfgdi.examplebeans.FakeDataSource;
 import com.github.cc007.sfgdi.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
-@PropertySource("classpath:jms.properties")
 public class PropertyConfig
 {
-	@Autowired
-	Environment environment;
-
 	@Value("${guru.username}")
 	String user;
 
@@ -40,7 +31,7 @@ public class PropertyConfig
 	public FakeDataSource fakeDataSource(){
 		FakeDataSource fakeDataSource = new FakeDataSource();
 		// or set the GURU_USERNAME env variable to set the guru.username value
-		fakeDataSource.setUser(environment.getProperty("USERNAME"));
+		fakeDataSource.setUser(user);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
@@ -49,15 +40,9 @@ public class PropertyConfig
 	@Bean
 	public FakeJmsBroker fakeJmsBroker(){
 		FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
-		// or set the GURU_USERNAME env variable to set the guru.username value
 		fakeJmsBroker.setUser(jmsUser);
 		fakeJmsBroker.setPassword(jmsPassword);
 		fakeJmsBroker.setUrl(jmsUrl);
 		return fakeJmsBroker;
-	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		return new PropertySourcesPlaceholderConfigurer();
 	}
 }
